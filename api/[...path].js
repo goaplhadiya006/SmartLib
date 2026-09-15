edba0bca2af2925cfc3b8d2d523f889a9e69e243
goaplhadiya006 <goaplahir006@gmail.com>
2026-09-15 12:49:32 +0530

    Fix Vercel Express API handler

--- api/[...path].js
+++ api/[...path].js
@@ -1,9 +1,3 @@
 const app = require("../server/server");
 
-module.exports = (req, res) => {
-  if (!req.url.startsWith("/api")) {
-    req.url = `/api${req.url.startsWith("/") ? "" : "/"}${req.url}`;
-  }
-
-  return app(req, res);
-};
+module.exports = app;
